ccc9d9a3e0eae5985ab848befb9cb8103c0cb2d8
Gayatri0903 <gayatri@aashman.in>
2025-11-22 16:00:21 +0530

    s

--- main/sensor.py
+++ main/sensor.py
@@ -1,11 +1,12 @@
 # sensor.py
-from vl53l1x import vl53l1x
+from vl53l1cx import VL53L1CX
 
 class DistanceSensor:
     def __init__(self):
         self.sensor = VL53L1X(i2c_bus=1, i2c_address=0x29)
         self.sensor.open()
         self.sensor.start_ranging()
+
 
     def read_cm(self):
         dist_mm = self.sensor.get_distance()
