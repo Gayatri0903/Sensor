c1f123efd5442be58efca0e9de6f645605c73fea
Gayatri0903 <gayatri@aashman.in>
2025-11-22 16:47:52 +0530

    s

--- main/sensor.py
+++ main/sensor.py
@@ -1,11 +1,12 @@
-from vl53l5cx import VL53L5CX
+from vl53l1x import VL53L1X
+
 import time
 
 class DistanceSensor:
     def __init__(self):
-        self.sensor = VL53L5CX()
+        self.sensor = VL53L1X()
         self.sensor.init()
-        print("VL53L5CX Sensor Ready")
+        print("VL53L1X Sensor Ready")
 
     def read_cm(self):
         data = self.sensor.get_ranging_data()
