db1e38adc5559012f5df807a06b2c96479cf5489
Gayatri0903 <gayatri@aashman.in>
2025-11-24 17:49:50 +0530

    new

--- rawintodist.py
+++ rawintodist.py
@@ -4,40 +4,45 @@ import time
 bus = smbus2.SMBus(1)
 addr = 0x29
 
-# ---- VL53L0X Simple Initialization ----
-def write(reg, value):
-    bus.write_byte_data(addr, reg, value)
+# -------------------------------------------------
+# HELPER FUNCTIONS
+# -------------------------------------------------
+def write(reg, val):
+    bus.write_byte_data(addr, reg, val)
 
-def read_byte(reg):
+def read(reg):
     return bus.read_byte_data(addr, reg)
 
 def read_word(reg):
-    msb = bus.read_byte_data(addr, reg)
-    lsb = bus.read_byte_data(addr, reg + 1)
-    return (msb << 8) | lsb
+    high = bus.read_byte_data(addr, reg)
+    low  = bus.read_byte_data(addr, reg + 1)
+    return (high << 8) | low
 
-# Enable sensor (mandatory)
-write(0x88, read_byte(0x88) | 0x01)
+# -------------------------------------------------
+# MINIMAL VL53L0X STARTUP (NO 0x88 REQUIRED)
+# -------------------------------------------------
 
-# Start continuous ranging
+# Put sensor into "standard ranging" mode
+write(0x00, 0x01)       # Fresh boot
+time.sleep(0.005)
+
+# Set continuous ranging mode
 write(0x80, 0x01)
 write(0xFF, 0x01)
 write(0x00, 0x00)
-write(0x91, read_byte(0x91))
+write(0x91, read(0x91))  # uses stored calibration
 write(0x00, 0x01)
 write(0xFF, 0x00)
 write(0x80, 0x00)
 
-# Command: continuous mode
-write(0x00, 0x04)
+write(0x00, 0x04)  # start continuous ranging
 
-print("VL53L0X running...")
+print("VL53L0X started. Reading distance...")
 
-# ---- Continuous Read Loop ----
+# -------------------------------------------------
+# CONTINUOUS READ
+# -------------------------------------------------
 while True:
-    distance_mm = read_word(0x14)  # Correct distance register
-    distance_cm = distance_mm / 10.0
-
-    print(f"Distance: {distance_mm} mm   {distance_cm:.2f} cm")
-
-    time.sleep(0.05)  # 20 Hz
+    d_mm = read_word(0x14)   # distance register
+    print("Distance:", d_mm, "mm  |", d_mm/10, "cm")
+    time.sleep(0.05)
