eb0f80e06c496270e88190feb2886ab034982e24
Gayatri0903 <gayatri@aashman.in>
2025-11-24 16:29:54 +0530

    new

--- rawdata.py
+++ rawdata.py
@@ -2,15 +2,25 @@ import smbus2
 import time
 
 bus = smbus2.SMBus(1)
-address = 0x29  # VL53L0X
+addr = 0x29
 
-# Start measurement (VL53L0X needs init)
-bus.write_byte_data(address, 0x00, 0x01)  # sysrange start
+# Mandatory init for VL53L0X
+def init_vl53l0x():
+    bus.write_byte_data(addr, 0x88, 0x00)
+    bus.write_byte_data(addr, 0x80, 0x01)
+    bus.write_byte_data(addr, 0xFF, 0x01)
+    bus.write_byte_data(addr, 0x00, 0x00)
+    bus.write_byte_data(addr, 0x91, bus.read_byte_data(addr, 0x91))
+    bus.write_byte_data(addr, 0x00, 0x01)
+    bus.write_byte_data(addr, 0xFF, 0x00)
+    bus.write_byte_data(addr, 0x80, 0x00)
+
+init_vl53l0x()
+
+# Start measurement
+bus.write_byte_data(addr, 0x00, 0x02)  # sysrange start continuous mode
 
 while True:
-    # Read RAW result block (0x14 → 14 bytes)
-    raw = bus.read_i2c_block_data(address, 0x14, 14)
-
-    print("RAW DATA:", raw)
-
+    raw = bus.read_i2c_block_data(addr, 0x14, 14)
+    print("RAW:", raw)
     time.sleep(0.1)
