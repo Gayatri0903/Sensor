f861e0641b4b2f3677661f39a41ad4fb95690d7f
Gayatri0903 <gayatri@aashman.in>
2025-11-24 16:02:49 +0530

    raww

---
+++ rawdata.py
@@ -0,0 +1,18 @@
+from smbus2 import SMBus
+import time
+
+I2C_ADDRESS = 0x29
+REGISTER = 0x01
+
+bus = SMBus(1)
+
+print("Reading raw I2C data... Press CTRL+C to stop.\n")
+
+try:
+    while True:
+        raw_byte = bus.read_byte_data(I2C_ADDRESS, REGISTER)
+        print(f"Register 0x{REGISTER:02X} -> Raw = {raw_byte:02X} ({raw_byte})")
+        time.sleep(0.1)
+
+except KeyboardInterrupt:
+    print("\nStopped by user.")
